ee74e8424cb6e2bd78fbbc627dc4860c405e6f3c
Martin Petr <git@bodkan.net>
2025-01-30 10:57:14 +0100

    Add dependencies

---
+++ dependencies.R
@@ -0,0 +1,8 @@
+# This script contains everything that's required to solve every exercise
+# in the activity based on the solutions provided in the exercises.qmd file.
+
+install.packages(c("slendr", "combinat", "cowplot", "dplyr", "readr",
+                   "tidyr", "ggplot2", "rmarkdown", "yaml"))
+
+library(slendr)
+setup_env(agree = TRUE)
