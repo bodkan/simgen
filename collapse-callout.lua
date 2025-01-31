508b9df232cf6d67e677e87b0ee750249f2e8f6a
Martin Petr <git@bodkan.net>
2025-01-31 16:30:02 +0100

    Split slendr document into subchapters

--- collapse-callout.lua
+++ collapse-callout.lua
@@ -1,3 +1,7 @@
+-- TODO: Looks at these links to define a custom "Show the solution" callout
+-- https://www.youtube.com/watch?v=DDQO_3R-q74
+-- https://examples.quarto.pub/collapse-callout/
+
 function Callout(el)
   if quarto.doc.isFormat("html") then
     -- Set default collapse to true if unset
