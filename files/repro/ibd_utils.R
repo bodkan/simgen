9ebeefbc9bcbbc33b361b79ddbaa467fa1eaedd1
Martin Petr <git@bodkan.net>
2025-09-26 11:09:42 +0200

    Add a reproducibility check

---
+++ files/repro/ibd_utils.R
@@ -0,0 +1,60 @@
+library(ggplot2) # the star of the day!
+
+library(dplyr)
+library(tidyr)
+library(readr)
+
+process_ibd <- function() {
+  cat("Downloading and processing IBD data...\n")
+
+  gz_file <- tempfile()
+  download.file("https://tinyurl.com/simgen-ibd", gz_file, mode = "wb", quiet = TRUE)
+  ibd_all <- read_tsv(gz_file, show_col_types = FALSE)
+
+  ibd <- ibd_all %>% mutate(length = end - start)
+
+  return(ibd)
+}
+
+process_metadata <- function() {
+  cat("Downloading and processing metadata...\n")
+
+  metadata_all <- read_tsv("https://tinyurl.com/simgen-metadata", show_col_types = FALSE)
+
+  # select a subset of columns
+  metadata <- select(metadata_all, sample = sampleId, country, age = ageAverage)
+  # replace missing ages of present-day individuals with 0
+  metadata <- mutate(metadata, age = if_else(is.na(age), 0, age))
+  # ignore archaic individuals
+  metadata <- filter(metadata, !sample %in% c("Vindija33.19", "AltaiNeandertal", "Denisova"))
+
+  # bin individuals according to their age
+  metadata$age_bin <- cut(metadata$age, breaks = seq(0, 50000, by = 5000), dig.lab = 10)
+  metadata <- metadata %>%
+    mutate(
+      age_bin = as.character(age_bin),
+      age_bin = if_else(is.na(age_bin), "present-day", age_bin)
+    )
+
+  return(metadata)
+}
+
+join_metadata <- function(ibd, metadata) {
+  cat("Joining IBD data and metadata...\n")
+
+  # prepare metadata for IBD annotation
+  metadata1 <- metadata
+  metadata2 <- metadata
+  colnames(metadata1) <- paste0(colnames(metadata1), "1")
+  colnames(metadata2) <- paste0(colnames(metadata2), "2")
+
+  # join based on sample1
+  ibd <- inner_join(ibd, metadata1)
+  # join based on sample2
+  ibd <- inner_join(ibd, metadata2)
+
+  # annotate with a new column indicating a pair of countries
+  ibd <- mutate(ibd, pair = paste(country1, country2, sep = "-"), .before = chrom)
+
+  return(ibd)
+}
