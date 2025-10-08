11f7d4e4fb156aa45294155a4b116bb1b5e71c43
Martin Petr <git@bodkan.net>
2025-10-08 23:46:58 +0200

    Add comments

--- files/repro/03_plot_metadata.R
+++ files/repro/03_plot_metadata.R
@@ -4,6 +4,7 @@ library(readr)
 
 metadata <- read_tsv("data/processed/metadata.tsv", show_col_types = FALSE)
 
+# create the barplot of counts of individuals in each age bin
 p_counts_bar <- metadata %>%
   ggplot(aes(x = age_bin)) +
   geom_bar() +
@@ -11,17 +12,22 @@ p_counts_bar <- metadata %>%
   ylab("Number of individuals") +
   ggtitle("Distribution of sample counts in each time period")
 
+# create a variation of the same barplot, with y-axis on the log-scale
 p_counts_bar_log <- p_counts_bar + scale_y_log10()
 
+# create a histogram of coverages in each age bin
 p_cov_hist <- metadata %>%
   ggplot(aes(coverage, fill = age_bin)) +
   geom_histogram() +
   facet_wrap(~ age_bin, scales = "free_y")
 
+# create an empty PDF file
 pdf("figures/metadata.pdf", width = 8, height = 6)
 
+# add figures to the PDF
 print(p_counts_bar)
 print(p_counts_bar_log)
 print(p_cov_hist)
 
+# close the PDF file
 dev.off()
